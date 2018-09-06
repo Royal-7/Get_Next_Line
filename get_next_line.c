a7b95930632051c3536b6cb940013e8eba3b7340
Royal-7 <n.r.bao11@gmail.com>
2018-09-06 13:32:02 -0700

    Add files via upload

---
+++ get_next_line.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abao <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/09/01 17:39:15 by abao              #+#    #+#             */
+/*   Updated: 2018/09/06 13:28:46 by abao             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+** Read from file descriptor until either new line or end of file.
+** Param: int file descriptor, char** to save the line.
+** Return: 1 if line has been read, 0 for EOF, -1 for error.
+*/
+
+#include <stdlib.h>
+
+int	get_next_line(const int fd, char **line)
+{
+
+	return (-1);
+}
---
+++ get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abao <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/09/04 13:49:40 by abao              #+#    #+#             */
+/*   Updated: 2018/09/04 13:50:29 by abao             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <libft.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+# define BUFF_SIZE 1
+
+int		get_next_line(const int fd, char **line);
+
+#endif
