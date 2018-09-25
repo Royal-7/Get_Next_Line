33b0e579281274aa55c25102a3467c623d820ad3
Royal-7 <n.r.bao11@gmail.com>
2018-09-24 17:43:17 -0700

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: abao <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/01 17:39:15 by abao              #+#    #+#             */
-/*   Updated: 2018/09/06 13:28:46 by abao             ###   ########.fr       */
+/*   Updated: 2018/09/24 17:40:00 by abao             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,34 @@
 ** Return: 1 if line has been read, 0 for EOF, -1 for error.
 */
 
+#include <stdio.h>
 #include <stdlib.h>
+#include "get_next_line.h"
 
 int	get_next_line(const int fd, char **line)
 {
+	char	buf[BUFF_SIZE + 1];
+	char	*newline;
+	int		x;
 
-	return (-1);
+	if (fd < 0 || line == NULL)
+		return (-1);
+	*line = malloc(sizeof(char) * BUFF_SIZE);
+	while ((x = read(fd, buf, BUFF_SIZE)) > 0)
+	{
+		buf[x] = '\0';
+		*line = ft_strjoin(*line, buf);
+		newline = ft_strchr(buf, '\n');
+		if (newline != NULL)
+		{
+			ft_strreplace(*line, '\n', '\0');
+			break ;
+		}
+		ft_strclr(buf);
+	}
+	if (x == 0)
+		return (0);
+	if (x == -1)
+		return (-1);
+	return (1);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,14 @@
 /*   By: abao <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/04 13:49:40 by abao              #+#    #+#             */
-/*   Updated: 2018/09/04 13:50:29 by abao             ###   ########.fr       */
+/*   Updated: 2018/09/16 16:17:25 by abao             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# include <libft.h>
+
+# include "libft/libft.h"
 # include <stdlib.h>
 # include <unistd.h>
 
